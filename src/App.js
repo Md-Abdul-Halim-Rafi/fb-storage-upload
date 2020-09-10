2406c01da10e7c3356b37ab80dbe9e4f98c793f3
Md-Abdul-Halim-Rafi <mdabdulhalimrafi@email.com>
2020-09-10 11:45:57 +0600

    storage upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { storage } from "./firebase";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState(
+    "Choose before Pressing the Upload button"
+  );
+  const [file, setFile] = useState(null);
+
+  const onFileChange = (event) => {
+    // Update the state
+    setFile(event.target.files[0]);
+  };
+
+  const onFileUpload = () => {
+    setLoading(true);
+
+    // Create an object of formData
+    const formData = new FormData();
+
+    // Update the formData object
+    formData.append("myFile", file, file.name);
+
+    // Details of the uploaded file
+    console.log(file);
+    const type = file.type.split(`/`)[0];
+    const storageRef = storage.ref(type);
+
+    storageRef
+      .child(`/${file.name}`)
+      .put(file)
+      .then(function (snapshot) {
+        setLoading(false);
+        setFile(null);
+        setMessage("Upload success!");
+        console.log("Uploaded a blob or file!", snapshot);
+      })
+      .catch((err) => {
+        console.log("Uploading error: ", err);
+        setFile(null);
+        setLoading(false);
+        setMessage("Upload failed!");
+      });
+  };
+
+  const fileData = () => {
+    if (file) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {file.name}</p>
+          <p>File Type: {file.type}</p>
+          <p>Last Modified: {file.lastModifiedDate.toDateString()}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>{message}</h4>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div>
+          <input type='file' onChange={onFileChange} />
+          {file && (
+            <button className='buttonload' onClick={onFileUpload}>
+              {loading ? (
+                <div>
+                  <i className='fa fa-circle-o-notch fa-spin'></i> Loading
+                </div>
+              ) : (
+                "Upload!"
+              )}
+            </button>
+          )}
+        </div>
+        {fileData()}
       </header>
     </div>
   );
---
+++ src/fbConfigExample.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "your_apiKey",
+  authDomain: "your_authDomain",
+  databaseURL: "your_databaseURL",
+  projectId: "your_projectId",
+  storageBucket: "your_storageBucket",
+  messagingSenderId: "your_messagingSenderId",
+  appId: "your_appId",
+  measurementId: "your_measurementId",
+};
